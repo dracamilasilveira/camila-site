d739778682b9f54351cf0dfadd2e46a7b1412f7d
Jonathas Silveira <joninhaevang@gmail.com>
2022-02-05 20:55:01 -0300

    Exapandindo e implementando os marcos do desenvolvimento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
+interface Link {
+  name: string,
+  routerLink: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+
   title = 'camila-site';
+  
+  links: Array<Link> = [
+    { 'name': 'Início', 'routerLink': '' },
+    { 'name': 'Marcos do desenvolvimento', 'routerLink': 'marcos-do-desenvolvimento' },
+    { 'name': 'Paralisia Cerebral', 'routerLink': 'paralisia-cerebral' },
+    { 'name': 'Sobre mim', 'routerLink': 'sobre-mim' }
+  ]
+
+  ages: Array<Link> = [
+    { 'name': '0-3 meses', 'routerLink': 'marcos-do-desenvolvimento' },
+    { 'name': '4-6 meses', 'routerLink': 'paralisia-cerebral' },
+    { 'name': '7-9 meses', 'routerLink': 'sobre-mim' }
+  ]
 }
--- src/app/growth-development/growth-development.component.ts
+++ src/app/growth-development/growth-development.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Link {
+  name: string,
+  routerLink: string;
+}
+
 @Component({
   selector: 'app-growth-development',
   templateUrl: './growth-development.component.html',
   styleUrls: ['./growth-development.component.css']
 })
 export class GrowthDevelopmentComponent implements OnInit {
-
+  ages: Array<Link> = [
+    { 'name': '0-3 meses', 'routerLink': 'marcos-do-desenvolvimento' },
+    { 'name': '4-6 meses', 'routerLink': 'paralisia-cerebral' },
+    { 'name': '7-9 meses', 'routerLink': 'sobre-mim' },
+    { 'name': '7-9 meses', 'routerLink': 'sobre-mim' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
